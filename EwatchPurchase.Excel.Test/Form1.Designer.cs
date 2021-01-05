fcbec10212e484e1e552a947dce3b56d30b4da95
kpleehuang <41355095+kpleehuang@users.noreply.github.com>
2021-01-05 17:09:57 +0800

    加入專案檔案。

---
+++ EwatchPurchase.Excel.Test/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace EwatchPurchase.Excel.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Importbutton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Importbutton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(918, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // Importbutton
+            // 
+            this.Importbutton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Importbutton.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Importbutton.Location = new System.Drawing.Point(804, 0);
+            this.Importbutton.Name = "Importbutton";
+            this.Importbutton.Size = new System.Drawing.Size(114, 50);
+            this.Importbutton.TabIndex = 0;
+            this.Importbutton.Text = "匯入檔案";
+            this.Importbutton.UseVisualStyleBackColor = true;
+            this.Importbutton.Click += new System.EventHandler(this.Importbutton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(918, 400);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Importbutton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ EwatchPurchase.Excel.Test/Form1.cs
@@ -0,0 +1,119 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EwatchPurchase.Excel.Test
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 檔案瀏覽
+        /// </summary>
+        public OpenFileDialog Openfile;
+        /// <summary>
+        /// 報表資料夾路徑
+        /// </summary>
+        public string ReportPath { get; set; }
+        /// <summary>
+        /// 設備名稱
+        /// </summary>
+        public string FieldName { get; set; }
+        /// <summary>
+        /// 檔案名稱完整路徑
+        /// </summary>
+        public string[] Header { get; set; }
+        /// <summary>
+        /// Ecexl檔案
+        /// </summary>
+        public XSSFWorkbook xworkbook { get; set; }//xlsx
+        public List<ICell> cell1 = new List<ICell>();
+        public List<ICell> cell2 = new List<ICell>();
+        public List<ICell> cell3 = new List<ICell>();
+        public List<ICell> cell4 = new List<ICell>();
+        public List<ICell> cell5 = new List<ICell>();
+        public List<ICell> cell6 = new List<ICell>();
+        public List<ICell> cell7 = new List<ICell>();
+        public List<ICell> cell8 = new List<ICell>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Importbutton_Click(object sender, EventArgs e)
+        {
+            Openfile = new OpenFileDialog() { Filter = "*.Xlsx| *.xlsx" };
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ReportPath = Openfile.FileName;
+                    if (ReportPath != null)
+                    {
+                        using (FileStream file = new FileStream($"{ReportPath}", FileMode.Open, FileAccess.Read))
+                        {
+                            xworkbook = new XSSFWorkbook(file);//Ecexl檔案載入
+                            int sheet = xworkbook.NumberOfSheets;//取得分頁數量
+                            for (int Sheetnum = 1; Sheetnum < 2; Sheetnum++)
+                            {
+                                var data = xworkbook.GetSheetAt(Sheetnum);//載入分頁資訊
+                                for (int Rownum = 9; Rownum < data.LastRowNum; Rownum++)//每一行資料
+                                {
+                                    IRow row = data.GetRow(Rownum);
+                                    cell1.Add(row.GetCell(0));
+                                    cell2.Add(row.GetCell(1));
+                                    cell3.Add(row.GetCell(2));
+                                    cell4.Add(row.GetCell(3));
+                                    if (row.GetCell(4).CellType == CellType.Formula)
+                                    {
+                                        row.GetCell(4).SetCellType(CellType.String);
+                                        cell5.Add(row.GetCell(4));
+                                    }
+                                    else
+                                    {
+                                        cell5.Add(row.GetCell(4));
+                                    }
+                                    if (row.GetCell(5).CellType == CellType.Formula)
+                                    {
+                                        row.GetCell(5).SetCellType(CellType.String);
+                                        cell6.Add(row.GetCell(5));
+                                    }
+                                    else
+                                    {
+                                        cell6.Add(row.GetCell(5));
+                                    }
+                                    cell7.Add(row.GetCell(6));
+                                    cell8.Add(row.GetCell(7));
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (DirectoryNotFoundException ex) { Log.Error(ex, $" [{ReportPath}] KWH查無此資料夾路徑"); }
+                catch (FileNotFoundException ex) { Log.Error(ex, $"KWH查無此資料檔案"); }
+                catch (Exception ex) { Log.Error(ex, $"KWH資料匯入失敗  檔案名稱{FieldName}"); }
+            }
+            dataGridView1.ColumnCount = 8;
+            dataGridView1.Columns[0].Name = Convert.ToString(cell1[0]);
+            dataGridView1.Columns[1].Name = Convert.ToString(cell2[0]);
+            dataGridView1.Columns[2].Name = Convert.ToString(cell3[0]);
+            dataGridView1.Columns[3].Name = Convert.ToString(cell4[0]);
+            dataGridView1.Columns[4].Name = Convert.ToString(cell5[0]);
+            dataGridView1.Columns[5].Name = Convert.ToString(cell6[0]);
+            dataGridView1.Columns[6].Name = Convert.ToString(cell7[0]);
+            dataGridView1.Columns[7].Name = Convert.ToString(cell8[0]);
+            for (int i = 1; i < cell1.Count; i++)
+            {
+                dataGridView1.Rows.Add(cell1[i], cell2[i], cell3[i], cell4[i], cell5[i], cell6[i], cell7[i], cell8[i]);
+            }
+        }
+    }
+}

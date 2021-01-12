0e03a46889fed6e097247a1231979d623c27ed93
kpleehuang <41355095+kpleehuang@users.noreply.github.com>
2021-01-12 14:30:09 +0800

    新增編碼原則資料庫。

---
+++ EwatchPurchase.SQL.Test/EF_Model/PurchaseProcessSystemDBModel/EncodeRules.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwatchPurchase.SQL.Test.EF_Model.PurchaseProcessSystemDBModel
+{
+    public partial class EncodeRules
+    {
+        public int pk { get; set; }
+        public string Code { get; set; }
+        public string CodeMeam { get; set; }
+    }
+}
